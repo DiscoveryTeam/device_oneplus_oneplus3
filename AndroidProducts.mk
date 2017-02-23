DiscoveryTeam/device_oneplus_oneplus3

commit f1f5a87ad764d5653d37fbb5faaae56165d12f11

OneplusDoze: don't use CM's stuff
Inspired by:

https://github.com/AOSP-JF-MM/platform_device_xiaomi_gemini/tree/aosp-7.1.1

device.mk
@@ -144,7 +144,19 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
+<<<<<<< HEAD
 # ANT+
+=======
+# Browser
+#PRODUCT_PACKAGES += \
+    Gello
+
+# Camera
+PRODUCT_PACKAGES += \
+    Camera2
+
+# Device config scripts
+>>>>>>> 7a5f436... XiaomiDoze: don't use CM's stuff
 PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library \
@@ -180,7 +192,7 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
 # Doze mode
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     OneplusDoze
 
 # Fingerprint sensor

commit 99f9ce8f63d596dcd6f1c11c77e823a69fe96814

Adjust some little things
1- Resolved conflit caused by: https://github.com/DiscoveryTeam/device_oneplus_oneplus3/commit/f1f5a87ad764d5653d37fbb5faaae56165d12f11
2- Added AndroidProducts.mk from kenzo

AndroidProducts.mk
@@ -1,0 +1,17 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/discovery_oneplus3.mk \

device.mk
@@ -144,19 +144,11 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
-<<<<<<< HEAD
-# ANT+
-=======
-# Browser
-#PRODUCT_PACKAGES += \
-    Gello
-
 # Camera
 PRODUCT_PACKAGES += \
     Camera2
 
 # Device config scripts
->>>>>>> 7a5f436... XiaomiDoze: don't use CM's stuff
 PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library \